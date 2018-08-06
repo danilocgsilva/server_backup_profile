928542ae271c4c7f597bc1e3c9467b4d703da649
Danilo Silva <danilocgsilva@gmail.com>
2018-08-06 12:32:09 -0300

    Refactoring for easing the reading

--- sbprofile.sh
+++ sbprofile.sh
@@ -22,33 +22,31 @@ check_if_profile_exists () {
   fi
 }
 
-create_profile_file () {
-
+##
+create_profile_folder () {
   if [ ! -f $profile_folder ]
   then
     mkdir -p $profile_folder
   fi
+}
 
-  local full_pem_file_path
-
+##
+ask_variables () {
   read -p "Enter the server name: " server_name
   read -p "Enter the user name: " user_name
   read -p "Enter the full pem file path, if any: " full_pem_file_path
   read -p "Enter the server path to your application: " application_server_path
+}
 
+##
+write_to_file () {
   write_entry_to_profile_path server_name
   write_entry_to_profile_path user_name
   write_entry_to_profile_path full_pem_file_path
   write_entry_to_profile_path application_server_path
-
-  make_download $profile_name
 }
 
-ask_variable () {
-  read -p "$1" $2
-  echo ${!2}
-}
-
+##
 write_entry_to_profile_path () {
   if [ ! -z $1 ]
   then
@@ -56,24 +54,32 @@ write_entry_to_profile_path () {
   fi
 }
 
+##
 make_download () {
   echo The file will be downloaded.
 }
 
 ## Main function
 sbprofile () {
+
   local profile_folder
   local profile_name
   local full_file_path
+  local full_pem_file_path
+  local server_name
+  local user_name
+  local application_server_path
+
   profile_folder=$(return_profile_folder)
   read -p "Type a profile name: " profile_name
   full_file_path=$profile_folder'/'$profile_name
-  if [ $(check_if_profile_exists $full_file_path) = 1 ]
+  if [ $(check_if_profile_exists $full_file_path) = 0 ]
   then
-    make_download $profile_name
-  else
-    create_profile_file
+    create_profile_folder
+    ask_variables
+    write_to_file
   fi
+  make_download $profile_name
 }
 
 ## detect if being sourced and
